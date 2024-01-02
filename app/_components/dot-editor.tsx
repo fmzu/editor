9ad6497ca8a93c22d11dee8af696a94168fe2e88
fmzu <fjtmzk0723@gmail.com>
2024-01-03 05:33:29 +0900

    update: ページを修正する

--- app/_components/dot-editor.tsx
+++ app/_components/dot-editor.tsx
@@ -13,7 +13,7 @@ const DEFAULT_COLOR = "#000000" // 黒
 
 // セルの型を定義します
 type Cell = {
-  color: string
+  color: string | null
 }
 
 // グリッドの初期状態を作成します
@@ -22,7 +22,7 @@ const createEmptyGrid = (): Cell[][] => {
   for (let i = 0; i < NUM_ROWS; i++) {
     rows.push([])
     for (let j = 0; j < NUM_COLS; j++) {
-      rows[i].push({ color: DEFAULT_COLOR })
+      rows[i].push({ color: null })
     }
   }
   return rows
@@ -31,48 +31,63 @@ const createEmptyGrid = (): Cell[][] => {
 export const DotEditor = () => {
   const [grid, setGrid] = useState(createEmptyGrid())
 
-  const [selectedColor, setSelectedColor] = useState(DEFAULT_COLOR)
+  const [colorId, setColorId] = useState("04")
 
-  const COLORS = Array.from(colors.values())
+  const COLORS = Array.from(colors.keys())
 
   const handleCellClick = (rowIndex: number, colIndex: number) => {
     const newGrid = [...grid]
-    newGrid[rowIndex][colIndex].color = selectedColor
+    newGrid[rowIndex][colIndex].color = colorId
     setGrid(newGrid)
   }
 
+  const gridToString = (grid: Cell[][]): string => {
+    return grid.map((row) => row.map((cell) => cell.color).join("-")).join("-")
+  }
+
   return (
-    <div className="flex gap-4 p-4">
-      <div className="flex flex-col gap-2 w-40">
-        {COLORS.map((color) => (
-          <div key={color} className="flex gap-2">
-            <div className="w-2" style={{ backgroundColor: color }} />
-            <Button
-              key={color}
-              variant={selectedColor === color ? "default" : "outline"}
-              onClick={() => setSelectedColor(color)}
-            >
-              {color}
-            </Button>
-          </div>
-        ))}
-      </div>
-      <div>
-        <Card>
-          {grid.map((row, rowIndex) => (
-            <div key={rowIndex} className="flex">
-              {row.map((cell, colIndex) => (
-                <button
-                  key={colIndex}
-                  type="button"
-                  onClick={() => handleCellClick(rowIndex, colIndex)}
-                  className={cn("w-8 h-8 border")}
-                  style={{ backgroundColor: cell.color }}
-                />
-              ))}
+    <div className="p-4 space-y-4">
+      <Card>
+        <pre>{gridToString(grid)}</pre>
+      </Card>
+      <div className="flex gap-4">
+        <div className="grid grid-cols-4 gap-2">
+          {COLORS.map((color) => (
+            <div key={color} className="flex gap-2">
+              <div
+                className="w-2"
+                style={{ backgroundColor: colors.get(color) }}
+              />
+              <Button
+                key={color}
+                variant={colorId === color ? "default" : "outline"}
+                onClick={() => setColorId(color)}
+              >
+                {color}
+              </Button>
             </div>
           ))}
-        </Card>
+        </div>
+        <div>
+          <Card>
+            {grid.map((row, rowIndex) => (
+              <div key={rowIndex} className="flex">
+                {row.map((cell, colIndex) => (
+                  <button
+                    key={colIndex}
+                    type="button"
+                    onClick={() => handleCellClick(rowIndex, colIndex)}
+                    className={cn("w-8 h-8 border")}
+                    style={{
+                      backgroundColor:
+                        cell.color !== null ? colors.get(cell.color) : "white",
+                    }}
+                  />
+                ))}
+              </div>
+            ))}
+          </Card>
+        </div>
       </div>
     </div>
   )
