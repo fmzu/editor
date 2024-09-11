d88bc65d875277b28344e3099a1891dd6df0eb6a
fmzu <149039422+fmzu@users.noreply.github.com>
2024-09-12 01:38:45 +0900

    update

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { cloudflareDevProxyVitePlugin, vitePlugin } from "@remix-run/dev"
+import { defineConfig } from "vite"
+import tsconfigPaths from "vite-tsconfig-paths"
+
+export default defineConfig({
+  plugins: [cloudflareDevProxyVitePlugin(), vitePlugin(), tsconfigPaths()],
+})
---
+++ worker-configuration.d.ts
@@ -0,0 +1,3 @@
+export type Env = {
+  AUTH_SECRET: string
+}
