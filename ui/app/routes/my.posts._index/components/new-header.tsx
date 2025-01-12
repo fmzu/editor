7deac6e481006776dd466678d9847c2335a20e41
shigurenimo <shigurenimo@gmail.com>
2025-01-12 10:06:17 +0900

    update

--- ui/app/routes/my.posts._index/components/new-header.tsx
+++ ui/app/routes/my.posts._index/components/new-header.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router"
 import { AvatarPopover } from "~/routes/_main/components/avatar-popover"
-import { PostDialog } from "~/routes/new/components/post-dialog"
+import { PostDialog } from "~/routes/my.posts._index/components/post-dialog"
 
 type Props = {
   onSubmit: () => void
